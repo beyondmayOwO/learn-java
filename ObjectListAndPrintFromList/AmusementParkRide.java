35ea21eb06148f3ea888652a665de6efd82344a9
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2025-05-26 20:07:08 +0630

    Object list and print from the list

---
+++ ObjectListAndPrintFromList/AmusementParkRide.java
@@ -0,0 +1,45 @@
+package ObjectListAndPrintFromList;
+
+import java.util.ArrayList;
+
+public class AmusementParkRide {
+    private String name;
+    private int minHeight;
+    private int visitors;
+    // List from another object as instance variable
+    private ArrayList<Person> riding;
+
+    public AmusementParkRide(String name, int minHeight) {
+        this.name = name;
+        this.minHeight = minHeight;
+        this.visitors = 0;
+        this.riding = new ArrayList<>();
+    }
+
+    // Allowed on the ride if the person is taller than minimum height
+    public boolean isAllowed(Person person) {
+        if(person.getHeight() < this.minHeight) {
+            return false;
+        }
+
+        this.visitors++;
+        this.riding.add(person);
+        return true;
+    }
+
+    // Return the String of allowed riders on the list
+    public String allRiding() {
+        String onTheRide = "";
+
+        for (Person person: riding) {
+            onTheRide = onTheRide + person.getName() + "\n";
+        }
+
+        return onTheRide;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", minimum height requirement: " + this.minHeight + ", visitors: " + this.visitors + "\n" + "Riding:\n " + allRiding();
+    }
+}
---
+++ ObjectListAndPrintFromList/Main.java
@@ -0,0 +1,14 @@
+package ObjectListAndPrintFromList;
+
+public class Main {
+    public static void main(String[] args) {
+        Person matt = new Person("Matt");
+        matt.setHeight(180);
+
+        AmusementParkRide train = new AmusementParkRide("Train Ride", 170);
+
+        train.isAllowed(matt);
+
+        System.out.println(train);
+    }
+}
