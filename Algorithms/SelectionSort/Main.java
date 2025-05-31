fb4c4d10d647643bf60aafea83caeabe8ebc4397
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2025-05-31 20:32:13 +0630

    Add SelectionSort directory

---
+++ Algorithms/SelectionSort/Main.java
@@ -0,0 +1,34 @@
+package Algorithms;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Arrays
+        int[] numbers = {8, 3, 7, 9, 1, 2, 4};
+        System.out.println(Arrays.toString(numbers));
+
+        // Sort the numbers
+        // toString only there for print
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        // Array lists
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(8);
+        numbers.add(3);
+        numbers.add(7);
+
+        System.out.println(numbers);
+
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
+}
+
+// Built-in sorting algorithms work for value-types variablse
+// But needs to modified for classes
