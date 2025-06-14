fc927fbc7a8284e5631092ed4fdb101ccb136507
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2025-06-14 21:50:00 +0630

    List, Map, Set and Collection

---
+++ ListMapSetCollection/Main.java
@@ -0,0 +1,40 @@
+package ListMapSetCollection;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // // ArrayList implements the List interface
+        // List <String> strings = new ArrayList<>();
+        // strings.add("Hello World!");
+
+        // System.out.println(strings);
+
+        // // Hashmap implements the Map interface
+        // Map<String, String> maps = new HashMap<>();
+        // maps.put("ganbatte", "good luck");
+        // maps.put("hai", "yes");
+
+        // for(String key: maps.keySet()) {
+        //     System.out.println(key + ": " + maps.get(key));
+        // }
+
+        // // In Java, sets always contain either 0 or 1 amounts of any given object.
+        // Set<String> set = new HashSet<>();
+        // set.add("one");
+        // set.add("one");
+        // set.add("two");
+
+        // for(String element: set) {
+        //     System.out.println(element);
+        // }
+
+        // Lists and sets are categorized as collections in Java — both the List and Set interfaces implement the Collection interface. 
+    }
+}
